5fdcde1746e2a4298f4ad58970e23a176443370d
HaoNP <haonp@splus-software.com.vn>
2025-05-26 14:53:58 +0700

    ES6 arrow function

---
+++ ES6/app.js
@@ -0,0 +1,10 @@
+// arrow function
+const sum = (a, b) => a + b;
+
+// arrow function dành cho object
+const sum2 = (a, b) => ({ a: a, b: b });
+
+/*
+Arrow function không hỗ trợ cho contructor
+Arrow function không hỗ trợ cho context trong object -> undefined
+*/
