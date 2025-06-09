ac3627b2ed2bade6e8d5e076cbbae9a8d918fce6
HaoNP <haonp@splus-software.com.vn>
2025-06-09 17:17:45 +0700

    Folder

--- my-app/src/MyForm.js
+++ my-app/src/MyForm.js
@@ -1,19 +1,40 @@
 import { useState } from "react";
 
 function MyForm() {
-    const [name, setName] = useState("");
+    const [inputs, setInputs] = useState({});
+
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setInputs(values => ({...values, [name]: value}))
+    }
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        alert(`The name you entered was: ${name}`)
+        console.log(inputs);
     }
 
     return (
         <form onSubmit={handleSubmit}>
             <label>Enter your name:
-                <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+                <input
+                    type="text"
+                    name="username"
+                    value={inputs.username || ""}
+                    onChange={handleChange}
+                />
             </label>
-            <input type="submit" value="Submit"/>
+
+            <label>Enter your age:
+                <input
+                    type="number"
+                    name="age"
+                    value={inputs.age || ""}
+                    onChange={handleChange}
+                />
+            </label>
+
+            <input type="submit" />
         </form>
     )
 }
