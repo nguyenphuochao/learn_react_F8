5c8b8e839f16aef49f5a0afcb22db8cf6d1ee5c8
HaoNP <haonp@splus-software.com.vn>
2025-06-12 18:01:20 +0700

    ToDoList

--- 6.Hooks/hook/src/App.js
+++ 6.Hooks/hook/src/App.js
@@ -1,17 +1,77 @@
 import { useState } from "react";
 
-
 function App() {
-  const [count, setCount] = useState(1);
+  // data
+  // const data = ["aaa", "bbb", "ccc"];
+  const storageJobs = JSON.parse(localStorage.getItem('jobs'));
 
-  const handleIncrease = () => {
-    setCount(count + 1);
+  const [job, setJob] = useState('');
+  const [jobs, setJobs] = useState(storageJobs ?? []);
+  const [feat, setFeat] = useState('Add');
+  const [index, setIndex] = useState(null);
+
+  const handleSubmit = () => {
+
+    if (feat == 'Add') {
+      setJobs(prev => {
+        const newJobs = [...prev, job];
+
+        // save to localstorage
+        const jsonJobs = JSON.stringify(newJobs);
+        localStorage.setItem('jobs', jsonJobs);
+
+        return newJobs
+      });
+    } else {
+      setJobs(prev => {
+        const newJobs = [...prev];
+        newJobs[index] = job;
+        // save to localstorage
+        const jsonJobs = JSON.stringify(newJobs);
+        localStorage.setItem('jobs', jsonJobs);
+
+        return newJobs
+      });
+    }
+
+    setJob('')
+  }
+
+  const handleDelete = (index) => {
+    setJobs(prev => {
+      const newJobs = [...prev];
+      newJobs.splice(index, 1);
+
+      const jsonJobs = JSON.stringify(newJobs);
+      localStorage.setItem('jobs', jsonJobs);
+
+      return newJobs
+    })
+  }
+
+  const handleEdit = (index) => {
+    setJob(jobs[index]);
+    setFeat('Edit');
+    setIndex(index);
   }
 
   return (
     <div className="App">
-      <h1>{count}</h1>
-      <button onClick={handleIncrease}>Increase</button>
+      <input value={job} onChange={e => setJob(e.target.value)} />
+      <button onClick={handleSubmit}>{feat}</button>
+
+      <ul>
+        {
+          jobs.map((job, index) =>
+            <>
+              <li onClick={() => handleEdit(index)} style={{ display: 'inline' }}>{job}</li>
+              <button onClick={() => handleDelete(index)}>Delete</button>
+              <br />
+            </>
+          )
+        }
+      </ul>
+
     </div>
   );
 }
