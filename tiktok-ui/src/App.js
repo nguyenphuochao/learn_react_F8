0615bfced6ea3785bfff484b9c66837faf5af113
HaoNP <haonp@splus-software.com.vn>
2025-06-26 15:46:02 +0700

    layout

--- tiktok-ui/src/App.js
+++ tiktok-ui/src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { publicRoutes } from './routes';
+import { DefaultLayout } from './components/Layout';
+import { Fragment } from 'react';
 
 function App() {
     return (
@@ -8,7 +10,16 @@ function App() {
                 <Routes>
                     {publicRoutes.map((route, index) => {
                         const Page = route.component;
-                        return <Route key={index} path={route.path} element={<Page />} />;
+
+                        // hanldle layout
+                        let Layout = DefaultLayout;
+                        if(route.layout) {
+                            Layout = route.layout
+                        }else if(route.layout === null) {
+                            Layout = Fragment
+                        }
+
+                        return <Route key={index} path={route.path} element={<Layout><Page /></Layout>} />;
                     })}
                 </Routes>
             </div>
---
+++ tiktok-ui/src/pages/Upload/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Upload() {
+    return (
+        <h1>Uploads</h1>
+    )
+}
+
+export default Upload
