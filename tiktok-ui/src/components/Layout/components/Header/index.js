ac6f988e323cfa87b1c54a41f8a2090a2de47db0
HaoNP <nguyenphuochao123@gmail.com>
2025-06-28 21:50:50 +0700

    header tiktok logo

--- tiktok-ui/src/components/Layout/components/Header/index.js
+++ tiktok-ui/src/components/Layout/components/Header/index.js
@@ -1,13 +1,27 @@
 import React from 'react'
 import classNames from 'classnames/bind'
 import style from './Header.module.scss'
+import images from '~/assets/images'
 
 const cx = classNames.bind(style)
 
 function Header() {
     return (
         <header className={cx('wrapper')}>
-            <div className={cx('inner')}></div>
+            <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt="tiktok" />
+                </div>
+                <div className={cx('search')}>
+                    <input placeholder="Search accounts and videos" spellCheck={false} />
+                    <button className={cx('clear')}>
+                        <i className="fas fa-times"></i>
+                    </button>
+                    <button className={cx('search-btn')}>
+                        <i className="fas fa-search"></i>
+                    </button>
+                </div>
+            </div>
         </header>
     )
 }
